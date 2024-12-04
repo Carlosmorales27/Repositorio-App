6cb75faa584d8f8935d733d06e9e01fae6dadafb
WilOuka21 <125584986+WilOuka21@users.noreply.github.com>
2024-12-03 21:37:19 -0600

    local auth
    se agregaron las verciones y librerias para poder hacer la autentificacion biometrica  se agrego el apartado de activar el boton de activar biometricos

--- login_app/lib/home_page.dart
+++ login_app/lib/home_page.dart
@@ -2,15 +2,61 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'form_page.dart'; // Página del formulario
 import 'submitted_forms_page.dart'; // Página de formularios enviados
+import 'package:local_auth/local_auth.dart';
 
 class HomePage extends StatelessWidget {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  final LocalAuthentication _localAuth = LocalAuthentication();
 
-  cons HomePage({Key? key}) : super(key: key);
+  HomePage({Key? key}) : super(key: key);
 
   void _logout(BuildContext context) async {
     await _auth.signOut();
     Navigator.pushReplacementNamed(context, '/');
+  }
+
+  Future<void> _navigateToFingerprintSettings(BuildContext context) async {
+    // Verificar si el dispositivo puede usar biometría
+    bool canCheckBiometrics = await _localAuth.canCheckBiometrics;
+    if (!canCheckBiometrics) {
+      _showErrorDialog(context, 'Este dispositivo no tiene soporte para huella digital.');
+      return;
+    }
+
+    // Informar al usuario si todo está bien
+    _showInfoDialog(context, 'Por favor, configura tu huella digital en los ajustes del dispositivo.');
+  }
+
+  void _showErrorDialog(BuildContext context, String message) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text('Error'),
+        content: Text(message),
+        actions: <Widget>[
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(),
+            child: const Text('OK'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _showInfoDialog(BuildContext context, String message) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text('Información'),
+        content: Text(message),
+        actions: <Widget>[
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(),
+            child: const Text('OK'),
+          ),
+        ],
+      ),
+    );
   }
 
   @override
@@ -24,24 +70,22 @@ class HomePage extends StatelessWidget {
         titleTextStyle: const TextStyle(color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
       ),
       body: Container(
-        color: Colors.grey[200], // Fondo claro
+        color: Colors.grey[200],
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            // Bienvenida ajustada
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 20),
               child: Text(
                 'Bienvenido, ${_auth.currentUser?.email ?? "Usuario"}',
                 style: const TextStyle(
-                  fontSize: 15, // Aumenté el tamaño de la fuente
+                  fontSize: 15,
                   fontWeight: FontWeight.bold,
-                  color: Colors.black, // Texto en negro
+                  color: Colors.black,
                 ),
                 textAlign: TextAlign.center,
               ),
             ),
-            // Tarjetas con botones
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
@@ -70,14 +114,28 @@ class HomePage extends StatelessWidget {
               ],
             ),
             const SizedBox(height: 30),
-            // Botón de cerrar sesión
+            ElevatedButton.icon(
+              onPressed: () => _navigateToFingerprintSettings(context),
+              icon: const Icon(Icons.fingerprint),
+              label: const Text('Configurar Huella Digital'),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.greenAccent,
+                foregroundColor: Colors.white,
+                padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                textStyle: const TextStyle(fontSize: 16),
+              ),
+            ),
+            const SizedBox(height: 30),
             ElevatedButton.icon(
               onPressed: () => _logout(context),
               icon: const Icon(Icons.logout),
               label: const Text('Cerrar Sesión'),
               style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.redAccent, // Fondo del botón
-                foregroundColor: Colors.white, // Color del texto e ícono
+                backgroundColor: Colors.redAccent,
+                foregroundColor: Colors.white,
                 padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(20),
@@ -91,7 +149,6 @@ class HomePage extends StatelessWidget {
     );
   }
 
-  // Widget para crear tarjetas estilizadas
   Widget _buildCard({
     required IconData icon,
     required Color color,
@@ -109,7 +166,7 @@ class HomePage extends StatelessWidget {
           padding: const EdgeInsets.all(15),
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(15),
-            color: Colors.white, // Fondo blanco para las tarjetas
+            color: Colors.white,
           ),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
@@ -121,7 +178,7 @@ class HomePage extends StatelessWidget {
                 style: const TextStyle(
                   fontSize: 16,
                   fontWeight: FontWeight.bold,
-                  color: Colors.black, // Texto en negro
+                  color: Colors.black,
                 ),
                 textAlign: TextAlign.center,
               ),
--- login_app/lib/login_page.dart
+++ login_app/lib/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:local_auth/local_auth.dart';
 import 'signup_page.dart';
 import 'home_page.dart';
 
@@ -14,248 +15,137 @@ class _LoginPageState extends State<LoginPage> {
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
   final FirebaseAuth _auth = FirebaseAuth.instance;
-  bool _isLoading = false; // Para mostrar el indicador de carga
+  final LocalAuthentication _localAuth = LocalAuthentication();
+  bool _isLoading = false;
 
-void _login() async {
-  String email = _emailController.text.trim();
-  String password = _passwordController.text.trim();
-
-  // Validar campos vacíos
-  if (email.isEmpty) {
-    showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-        title: const Text('Error'),
-        content: const Text('Por favor, complete el campo de correo'),
-        actions: <Widget>[
-          TextButton(
-            onPressed: () => Navigator.of(context).pop(),
-            child: const Text('OK'),
-          ),
-        ],
-      ),
-    );
-    return;
-  }
-
-  if (password.isEmpty) {
-    showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-        title: const Text('Error'),
-        content: const Text('Por favor, complete el campo de contraseña'),
-        actions: <Widget>[
-          TextButton(
-            onPressed: () => Navigator.of(context).pop(),
-            child: const Text('OK'),
-          ),
-        ],
-      ),
-    );
-    return;
-  }
-
-    if (email.isEmpty && password.isEmpty) {
-    showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-        title: const Text('Error'),
-        content: const Text('Por favor, complete todos los campos.'),
-        actions: <Widget>[
-          TextButton(
-            onPressed: () => Navigator.of(context).pop(),
-            child: const Text('OK'),
-          ),
-        ],
-      ),
-    );
-    return;
-  }
-
-  // Validar formato del correo electrónico
-  final emailRegex = RegExp(r'^[^@\s]+@[^@\s]+\.[^@\s]+$');
-  if (!emailRegex.hasMatch(email)) {
-    showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-        title: const Text('Error'),
-        content: const Text('El correo electrónico no tiene un formato válido.'),
-        actions: <Widget>[
-          TextButton(
-            onPressed: () => Navigator.of(context).pop(),
-            child: const Text('OK'),
-          ),
-        ],
-      ),
-    );
-    return;
-  }
-
-  // Mostrar indicador de carga
-  setState(() {
-    _isLoading = true;
-  });
-
-  // Intentar iniciar sesión
-  try {
-    UserCredential userCredential = await _auth.signInWithEmailAndPassword(
-      email: email,
-      password: password,
-    );
-    Navigator.pushReplacement(
-      context,
-      MaterialPageRoute(builder: (context) => HomePage()),
-    );
-  } on FirebaseAuthException catch (e) {
-    // Manejo de errores específicos de FirebaseAuth
-    String errorMessage;
-    switch (e.code) {
-      case 'user-not-found':
-        errorMessage = 'No existe ningún usuario con este correo.';
-        break;
-      case 'wrong-password':
-        errorMessage = 'La contraseña es incorrecta.';
-        break;
-      default:
-        errorMessage = 'Ocurrió un error inesperado. Intente nuevamente.';
+  // Verificar disponibilidad de autenticación biométrica
+  Future<void> _authenticateBiometric() async {
+    bool isAuthenticated = false;
+    try {
+      isAuthenticated = await _localAuth.authenticate(
+        localizedReason: 'Use su biometría para iniciar sesión',
+        options: const AuthenticationOptions(
+          biometricOnly: true,
+        ),
+      );
+    } catch (e) {
+      debugPrint("Error en la autenticación biométrica: $e");
     }
 
-    showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-        title: const Text('Error'),
-        content: Text(errorMessage),
-        actions: <Widget>[
-          TextButton(
-            onPressed: () => Navigator.of(context).pop(),
-            child: const Text('OK'),
-          ),
-        ],
-      ),
-    );
-  } catch (e) {
-    showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-        title: const Text('Error'),
-        content: const Text('Ocurrió un error inesperado.'),
-        actions: <Widget>[
-          TextButton(
-            onPressed: () => Navigator.of(context).pop(),
-            child: const Text('OK'),
-          ),
-        ],
-      ),
-    );
-  } finally {
-    // Detener indicador de carga
-    setState(() {
-      _isLoading = false;
-    });
+    if (isAuthenticated) {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => HomePage()),
+      );
+    } else {
+      showDialog(
+        context: context,
+        builder: (context) => const AlertDialog(
+          title: Text('Error'),
+          content: Text('La autenticación biométrica falló.'),
+        ),
+      );
+    }
   }
-}
+
+  void _login() async {
+    String email = _emailController.text.trim();
+    String password = _passwordController.text.trim();
+
+    // Validar campos vacíos y formato
+    if (email.isEmpty || password.isEmpty) {
+      showDialog(
+        context: context,
+        builder: (context) => const AlertDialog(
+          title: Text('Error'),
+          content: Text('Por favor, complete todos los campos.'),
+        ),
+      );
+      return;
+    }
+
+    // Mostrar indicador de carga
+    setState(() {
+      _isLoading = true;
+    });
+
+    try {
+      await _auth.signInWithEmailAndPassword(email: email, password: password);
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => HomePage()),
+      );
+    } on FirebaseAuthException catch (e) {
+      String errorMessage = 'Ocurrió un error inesperado.';
+      if (e.code == 'user-not-found') {
+        errorMessage = 'No existe ningún usuario con este correo.';
+      } else if (e.code == 'wrong-password') {
+        errorMessage = 'La contraseña es incorrecta.';
+      }
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: const Text('Error'),
+          content: Text(errorMessage),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: const Text('OK'),
+            ),
+          ],
+        ),
+      );
+    } finally {
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Login"),
-        backgroundColor: Colors.transparent, // AppBar transparente para un look más limpio
-        elevation: 0, // Eliminar la sombra del AppBar
       ),
-      body: Container(
+      body: Padding(
         padding: const EdgeInsets.all(16.0),
-        decoration: const BoxDecoration(
-          color: Colors.white, // Fondo blanco para un diseño más limpio
-        ),
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              // Título central
-              const Text(
-                '¡Hola, bienvenido!',
-                style: TextStyle(
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black, // Texto oscuro para contraste
-                ),
-              ),
-              const SizedBox(height: 40),
-
-              // Campo de correo electrónico
-              TextField(
-                controller: _emailController,
-                decoration: InputDecoration(
-                  labelText: 'Correo Electrónico',
-                  labelStyle: const TextStyle(color: Colors.grey),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8), // Bordes más suaves
-                    borderSide: const BorderSide(color: Colors.grey),
-                  ),
-                  filled: true,
-                  fillColor: Colors.grey[100], // Fondo claro y suave para el campo
-                ),
-              ),
-              const SizedBox(height: 20),
-
-              // Campo de contraseña
-              TextField(
-                controller: _passwordController,
-                obscureText: true,
-                decoration: InputDecoration(
-                  labelText: 'Contraseña',
-                  labelStyle: const TextStyle(color: Colors.grey),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8), // Bordes más suaves
-                    borderSide: const BorderSide(color: Colors.grey),
-                  ),
-                  filled: true,
-                  fillColor: Colors.grey[100], // Fondo claro y suave para el campo
-                ),
-              ),
-              const SizedBox(height: 40),
-
-              // Botón de login
-              _isLoading
-                  ? const CircularProgressIndicator() // Mostrar indicador de carga
-                  : ElevatedButton(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: _emailController,
+              decoration: const InputDecoration(labelText: 'Correo Electrónico'),
+            ),
+            const SizedBox(height: 10),
+            TextField(
+              controller: _passwordController,
+              obscureText: true,
+              decoration: const InputDecoration(labelText: 'Contraseña'),
+            ),
+            const SizedBox(height: 20),
+            if (_isLoading)
+              const CircularProgressIndicator()
+            else
+              ElevatedButton(
                 onPressed: _login,
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.blue, // Un color más brillante para el botón
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8), // Bordes suaves
-                  ),
-                  padding: const EdgeInsets.symmetric(vertical: 14),
-                ),
-                child: const Text(
-                  'Iniciar Sesión',
-                  style: TextStyle(
-                    fontSize: 18,
-                    color: Colors.white, // Color del texto en blanco para un buen contraste
-                  ),
-                ),
+                child: const Text('Iniciar Sesión'),
               ),
-              const SizedBox(height: 20),
-
-              // Botón para ir a la página de registro
-              TextButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => SignupPage()),
-                  );
-                },
-                child: const Text(
-                  '¿No tienes cuenta? Regístrate',
-                  style: TextStyle(
-                    color: Colors.blue, // Texto azul para el enlace
-                    fontSize: 16,
-                  ),
-                ),
-              ),
-            ],
-          ),
+            const SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: _authenticateBiometric,
+              child: const Text('Iniciar Sesión con Biometríca'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SignupPage()),
+                );
+              },
+              child: const Text('¿No tienes cuenta? Regístrate'),
+            ),
+          ],
         ),
       ),
     );
